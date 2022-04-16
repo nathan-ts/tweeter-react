07e9cb591e1b18d1c748f3e66bc7470a4a4f08ea
nathan-ts <42843617+nathan-ts@users.noreply.github.com>
2022-04-16 18:55:49 -0400

    add handler to tweetform

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -3,9 +3,14 @@ import React from 'react'; //optional
 function TweetForm(){
   const message = "What are you humming about?";
 
+  const handleSubmit = e => {
+    e.preventDefault();
+
+  };
+
   return (
     <section className="newtweet">
-      <form method="post" action="/tweets" className="newtweet__form">
+      <form onSubmit={handleSubmit} method="post" action="/tweets" className="newtweet__form">
         <textarea className="form__textarea" name="text" placeholder={message}></textarea>
         <input type="submit" value="Tweet" className="form__input" />
         <span className="form__counter">140</span>
